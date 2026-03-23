34fbd9eb72804b77402a3adc22425112969c78d5
RebecaTI <mebuffbeka@gmail.com>
2026-03-23 16:50:04 -0300

    feat: add Demo component and update App layout with flexbox

---
+++ src/hookComponent/1-useState/index.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+interface DemoProps {
+
+}
+
+export default function Demo({ }: DemoProps) {
+  const [count, setCount] = useState(0); //Dentro do useState é o estado inicial, ou seja, o valor que a variável de estado vai começar. No caso, 0. O useState retorna um array com dois elementos: o primeiro é a variável de estado (count) e o segundo é a função para atualizar essa variável (setCount). O array é desestruturado para obter essas duas variáveis.
+  return (
+    <div>
+      <h1>useState Simply Explained!</h1>
+      <button> Increment</button>
+    </div>
+  )
+}
